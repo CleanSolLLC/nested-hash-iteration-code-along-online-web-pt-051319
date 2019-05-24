09c1fb2d415e23c3b29d1a0756877ebca963aaa3
Mark May <mamark90@gmail.com>
2019-05-24 16:32:44 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,7 +16,7 @@ require 'pry'
 
 def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
-    contact_details_hash.each do |data, attributes|
+    contact_details_hash.each do |data, attribute|
       if attribute == :favorite_ice_cream_flavors
     binding.pry
       end
